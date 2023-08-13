498bd63546799e0825328467266e63e4d4c782ef
Kokkiligadda Neeraja <105647453+KokkiligaddaNeeraja@users.noreply.github.com>
2023-08-13 16:13:31 +0530

    main index

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Web Learning</title>
+	<style>
+		*{
+			padding:0;
+			border:0;
+		}
+	</style>
+</head>
+<body>
+	<?php
+	include "header.html";
+	?>
+	<?php
+	include "home.html";
+	?>
+	<?php
+	include "courses.html"
+	?>
+	<?php
+	include "contact.html";
+	?>
+</body>
+</html>
